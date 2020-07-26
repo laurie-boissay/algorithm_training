714b7f33b818b511a474565716cdab35abafe378
Jaenne <59830376+laurie-boissay@users.noreply.github.com>
2020-07-26 22:18:37 +0200

    Add files via upload

--- 9_balls/found_error_ball.py
+++ 9_balls/found_error_ball.py
@@ -6,8 +6,8 @@
 """
 I've eared about a problem :
 
-We have 9 balls all weigh the same except one.
-How find this different ball ?
+We have 9 balls all weigh the same except one that weigh more.
+How find this different ball with only 2 comparative weighings ?
 
 This program is my response to this question.
 """
@@ -35,13 +35,24 @@ group_3.append(all_balls[7])
 group_3.append(all_balls[8])
 
 
-def weigh_balls(group, normal_weight):
-	# Check all balls weight untill to find the error:
-	for ball in range(3):
-		# if ball weight != normal_weight:
-		if group[ball] != normal_weight :
-			# return error
-			return ball+1
+def weigh_balls(group):
+	# Weigh 2 balls 0 and 1:
+	# if weight ball 0 > weight ball 1:
+	if group[0] > group[1]:
+		# error = ball 0
+		error = 0
+
+	# if weight ball 0 == weight ball 1:
+	elif group[0] < group[1]:
+		# error = ball 1
+		error = 1
+	
+	else: #group[0] == group[1]
+		# error = ball 2
+		error = 2
+
+	return error + 1
+
 
 # Weigh group 1.
 weight_group_1 = group_1[0] + group_1[1] + group_1[2]
@@ -49,46 +60,30 @@ weight_group_1 = group_1[0] + group_1[1] + group_1[2]
 # Weigh group 2.
 weight_group_2 = group_2[0] + group_2[1] + group_2[2]
 
-# If weight group 1 = weight group 2:
-if weight_group_1 == weight_group_2:
-	# the error is in the group 3.
-	# normal_weight of a ball = weight group 1 /3.
-	normal_weight = weight_group_1 / 3
-	
-	# weigh each ball in group 3:
-	error_ball = weigh_balls(group_3, normal_weight) + 6
-
-	
-# else (weight group 1 != weight group 2):
-else:
-	# the error is not in the group 3.
-	# Weigh group 3.
-	weight_group_3 = group_3[0] + group_3[1] + group_3[2]
-
-	# normal weight of a ball = weight group 3 /3.
-	normal_weight = weight_group_3 / 3
-
-	# if weight group 1 != weight group 3:
-	if weight_group_1 != weight_group_3:
+# else if (weight group 1 > weight group 2):
+if weight_group_1 > weight_group_2:
 		# the error is in the group 1.
-		# weigh each ball in group 1:
-		error_ball = weigh_balls(group_1, normal_weight)
+		# weigh 2 balls in group 1:
+		error_ball = weigh_balls(group_1)
 
-	# else (weight_group_2 != weight group 3):
-	else:
+# else (weight group 1 < weight group 2):
+elif weight_group_1 < weight_group_2:
 		# the error is in the group 2.
-		# weigh each ball in group 2 :
-		error_ball = weigh_balls(group_2, normal_weight) + 3
+		# weigh 2 balls in group 2:
+		error_ball = weigh_balls(group_2) + 3
+
+else: # weight_group_1 == weight_group_2
+	# the error is in the group 3.
+	# weigh 2 balls in group 3:
+	error_ball = weigh_balls(group_3) + 6
 
 
 # Display error :
 print(f"\nThe number of the different ball is : {error_ball}.")
-
 
 print("\nLet's check the weight of each ball :\n")
 for i in range(9):
 	print(f"Ball n°{i+1} : {all_balls[i]} g.")
 
 print("\n")
-
 
--- 9_balls/generate_9_balls.py
+++ 9_balls/generate_9_balls.py
@@ -6,7 +6,7 @@
 """
 Functions used to generate 9 balls.
 8 balls weigh the same.
-1 ball has an error with an other weight.
+1 ball weigh more.
 
 Create a list with 9 balls corresponding to the description.
 """
@@ -18,14 +18,10 @@ from balls_list import all_balls
 
 
 def create_9_balls():
-	weight_error = weight = randrange(1,11)
-	
+	weight = randrange(1,11)
 	error_ball = randrange(0,9)
 	
 	for i in range(9):
 		all_balls.append(weight)
-
-	while weight_error == weight:
-		weight_error = randrange(1,11)
 	
-	all_balls[error_ball] = weight_error
+	all_balls[error_ball] = weight + randrange(1,11)
